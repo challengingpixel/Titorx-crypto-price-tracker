1f8fe1c9c0275acc8ac25f3ee4934a15d9d5433b
petartodorovski <petar.todorovski@newsroom.tech>
2021-04-24 02:23:40 +0200

    added firebase for deploying

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ4cy8CPUgkPSXrboQQEsnnRaCWM8hlCw",
+    authDomain: "titorx-crypto.firebaseapp.com",
+    projectId: "titorx-crypto",
+    storageBucket: "titorx-crypto.appspot.com",
+    messagingSenderId: "569245097324",
+    appId: "1:569245097324:web:da82dbb391f8c1eafe2749"
+};
